cf05c5fffb3bee26462c30b665e48dfb29f81f0f
Sahas Ramesh <>
2023-06-27 00:06:52 -0500

    Initial This Love commit

--- js/mirrs.js
+++ js/mirrs.js
@@ -1,19 +1,51 @@
-const audio = document.getElementById("audio");
-const playPauseBtn = document.getElementById("play-pause-btn");
+const playButton1 = document.getElementById('button button1');
+const playButton2 = document.getElementById('button button2');
+const audio1 = document.getElementById('audio1');
+const audio2 = document.getElementById('audio2');
 
-playPauseBtn.addEventListener("click", function () {
-  if (audio.paused) {
-    audio.play();
-    playPauseBtn.classList.remove("play");
-    playPauseBtn.classList.add("pause");
+let isAudio1Playing = false;
+let isAudio2Playing = false;
+
+playButton1.addEventListener('click', () => {
+  if (isAudio2Playing) {
+    audio2.pause();
+    audio2.currentTime = 0;
+    isAudio2Playing = false;
+  }
+  
+  if (!isAudio1Playing) {
+    audio1.play();
+    isAudio1Playing = true;
   } else {
-    audio.pause();
-    playPauseBtn.classList.remove("pause");
-    playPauseBtn.classList.add("play");
+    audio1.pause();
+    isAudio1Playing = false;
   }
 });
 
-audio.addEventListener("ended", function () {
-  playPauseBtn.classList.remove("pause");
-  playPauseBtn.classList.add("play");
+playButton2.addEventListener('click', () => {
+  if (isAudio1Playing) {
+    audio1.pause();
+    audio1.currentTime = 0;
+    isAudio1Playing = false;
+  }
+  
+  if (!isAudio2Playing) {
+    audio2.play();
+    isAudio2Playing = true;
+  } else {
+    audio2.pause();
+    isAudio2Playing = false;
+  }
+});
+
+audio1.addEventListener('ended', () => {
+  isAudio1Playing = false;
+  playButton1.textContent = 'Play Audio 1';
+  audio1.currentTime = 0;
+});
+
+audio2.addEventListener('ended', () => {
+  isAudio2Playing = false;
+  playButton2.textContent = 'Play Audio 2';
+  audio2.currentTime = 0;
 });

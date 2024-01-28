071bd495393bff01db75c956792ec56dc6b6467c
Sahas Ramesh <>
2024-01-27 19:31:19 -0800

    Added sparks fly

--- js/mirrs.js
+++ js/mirrs.js
@@ -1,13 +1,20 @@
 const playButton1 = document.getElementById('button button1');
 const playButton2 = document.getElementById('button button2');
 const playButton3 = document.getElementById('button button3');
+const playButton4 = document.getElementById('button button4');
+const playButton5 = document.getElementById('button button5');
+
 const audio1 = document.getElementById('audio1');
 const audio2 = document.getElementById('audio2');
 const audio3 = document.getElementById('audio3');
+const audio4 = document.getElementById('audio4');
+const audio5 = document.getElementById('audio5');
 
 let isAudio1Playing = false;
 let isAudio2Playing = false;
 let isAudio3Playing = false;
+let isAudio4Playing = false;
+let isAudio5Playing = false;
 
 playButton1.addEventListener('click', () => {
   if (isAudio2Playing) {
@@ -20,6 +27,18 @@ playButton1.addEventListener('click', () => {
     audio3.pause();
     audio3.currentTime = 0;
     isAudio3Playing = false;
+  }
+
+  if (isAudio4Playing) {
+    audio4.pause();
+    audio4.currentTime = 0;
+    isAudio4Playing = false;
+  }
+
+  if (isAudio5Playing) {
+    audio5.pause();
+    audio5.currentTime = 0;
+    isAudio5Playing = false;
   }
   
   if (!isAudio1Playing) {
@@ -43,6 +62,18 @@ playButton2.addEventListener('click', () => {
     audio3.currentTime = 0;
     isAudio3Playing = false;
   }
+
+  if (isAudio4Playing) {
+    audio4.pause();
+    audio4.currentTime = 0;
+    isAudio4Playing = false;
+  }
+
+  if (isAudio5Playing) {
+    audio5.pause();
+    audio5.currentTime = 0;
+    isAudio5Playing = false;
+  }
   
   if (!isAudio2Playing) {
     audio2.play();
@@ -65,6 +96,18 @@ playButton3.addEventListener('click', () => {
     audio2.currentTime = 0;
     isAudio2Playing = false;
   }
+
+  if (isAudio4Playing) {
+    audio4.pause();
+    audio4.currentTime = 0;
+    isAudio4Playing = false;
+  }
+
+  if (isAudio5Playing) {
+    audio5.pause();
+    audio5.currentTime = 0;
+    isAudio5Playing = false;
+  }
   
   if (!isAudio3Playing) {
     audio3.play();
@@ -72,6 +115,74 @@ playButton3.addEventListener('click', () => {
   } else {
     audio3.pause();
     isAudio3Playing = false;
+  }
+});
+
+playButton4.addEventListener('click', () => {
+  if (isAudio1Playing) {
+    audio1.pause();
+    audio1.currentTime = 0;
+    isAudio1Playing = false;
+  }
+
+  if (isAudio2Playing) {
+    audio2.pause();
+    audio2.currentTime = 0;
+    isAudio2Playing = false;
+  }
+
+  if (isAudio3Playing) {
+    audio3.pause();
+    audio3.currentTime = 0;
+    isAudio3Playing = false;
+  }
+
+  if (isAudio5Playing) {
+    audio5.pause();
+    audio5.currentTime = 0;
+    isAudio5Playing = false;
+  }
+  
+  if (!isAudio4Playing) {
+    audio4.play();
+    isAudio4Playing = true;
+  } else {
+    audio4.pause();
+    isAudio4Playing = false;
+  }
+});
+
+playButton5.addEventListener('click', () => {
+  if (isAudio1Playing) {
+    audio1.pause();
+    audio1.currentTime = 0;
+    isAudio1Playing = false;
+  }
+
+  if (isAudio2Playing) {
+    audio2.pause();
+    audio2.currentTime = 0;
+    isAudio2Playing = false;
+  }
+
+  if (isAudio4Playing) {
+    audio4.pause();
+    audio4.currentTime = 0;
+    isAudio4Playing = false;
+  }
+
+  if (isAudio3Playing) {
+    audio3.pause();
+    audio3.currentTime = 0;
+    isAudio3Playing = false;
+  }
+  
+  if (!isAudio5Playing) {
+    audio5.play();
+    isAudio5Playing = true;
+  } else {
+    audio5.pause();
+    isAudio5Playing = false;
   }
 });
 
@@ -88,4 +199,14 @@ audio2.addEventListener('ended', () => {
 audio3.addEventListener('ended', () => {
   isAudio3Playing = false;
   audio3.currentTime = 0;
+});
+
+audio4.addEventListener('ended', () => {
+  isAudio4Playing = false;
+  audio4.currentTime = 0;
+});
+
+audio5.addEventListener('ended', () => {
+  isAudio5Playing = false;
+  audio5.currentTime = 0;
 });
